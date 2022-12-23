09b286068f8d30b387a0ea466d1d113001992966
paola pesce <paojpesce@gmail.com>
2022-12-22 21:13:41 -0300

    avance

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -4,19 +4,29 @@ class PlayersController < ApplicationController
 
   def index
     # Endpoint para obtener todos los jugadores => http://localhost:3000/api/v1/players
-    result = RestClient.get('http://localhost:3000/api/v1/players')
-    @players = JSON.parse(result) #
+    result = RestClient.get('http://localhost:3001/api/v1/players')
+    @players = JSON.parse(result)
     # binding.break reemplaza al raise - hace un stop -
   end
 
   def show
-    #Endpoint para obtener un jugador => http://localhost:3000/api/v1/players
+    # Endpoint para obtener un jugador => http://localhost:3000/api/v1/players
+    result = RestClient.get("http://localhost:3001/api/v1/players/#{params[:id]}")
+    @player = JSON.parse(result)
   end
 
   def new
   end
 
   def create
+    result = RestClient.post('http://localhost:3001/api/v1/players', { 'name' => params['name'], 'team' => params['team'] }.to_json,
+    {content_type: :json, accept: :json} )
+
+    if result.code == 201
+      redirect_to players_path
+    else
+      render '/players/new', status: :unprocessable_entity
+    end
   end
 
   def edit
